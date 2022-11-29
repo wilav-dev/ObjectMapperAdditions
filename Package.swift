4207cce607179449b9ed807d082bcdae00534c97
wilav-dev <71793326+wilav-dev@users.noreply.github.com>
2022-11-29 18:51:32 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.32.0")),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "10.0.0")),
         .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.0.0")),
         .package(url: "https://github.com/APUtils/LogsManager.git", .upToNextMajor(from: "9.1.14")),
     ],
